1c570c6426fad8519941bdea24e335318da48c61
Jonathan Heathcote <mail@jhnet.co.uk>
2023-05-05 16:05:42 +0100

    First full prototype (static site only)

--- memories.py
+++ memories.py
@@ -1,6 +1,14 @@
+from typing import Any, NamedTuple
+
 from pathlib import Path
 
+from collections import OrderedDict
+
 from aiohttp import web
+
+import re
+
+import jinja2
 
 import sqlite3
 
@@ -11,102 +19,221 @@ from calendar import monthrange
 routes = web.RouteTableDef()
 
 
+def render_template(request: web.Request, template: str, variables: dict[str, Any] = {}) -> web.Response:
+    """
+    Render a Jinja2 template.
+    """
+    static = ("../" * (request.url.path.count("/") - 1)) + "static"
+    
+    template = request.app["env"].get_template(template)
+    page = template.render(static=static, **variables)
+    
+    return web.Response(body=page, content_type="text/html")
+
+
 @routes.get("/")
 async def index(request: web.Request) -> web.Response:
-    return web.FileResponse(Path(__file__).parent / "index.html")
-
-
-@routes.get("/api/names")
-async def get_names(request: web.Request) -> web.Response:
-    """(JSON) List of names memories are assigned to."""
     cur = request.app["db"].cursor()
     res = cur.execute("SELECT DISTINCT name FROM memories ORDER BY name")
-    return web.json_response([n[0] for n in res.fetchall()])
+    names = [n[0] for n in res.fetchall()]
+    
+    return render_template(request, "index.html", {"names": names})
 
 
-@routes.get(r"/api/memories/{name}")
-async def get_memories(request: web.Request) -> web.Response:
+class DateDelta(NamedTuple):
+    years: int
+    months: int
+    days: int
+
+
+def date_delta(before: datetime.date, after: datetime.date) -> DateDelta:
     """
-    Enumerate existing memories for a given name.
-    
-    A (oldest-to-newest) list of objects as follows:
-    
-        {"id": 123, "date": "yyyy-mm-dd", "name": "...", "note": "..."}
+    Return the difference between two dates in terms of calendar years, months
+    and days.
     """
-    cur = request.app["db"].cursor()
+    delta_months = ((after.year * 12) + after.month) - ((before.year * 12) + before.month)
+    delta_years = delta_months // 12
+    delta_months -= delta_years * 12
     
+    delta_days = after.day - before.day
+    if delta_days < 0:
+        delta_days += monthrange(before.year, before.month)[1]
+        delta_months -= 1
+    if delta_months < 0:
+        delta_months += 12
+        delta_years -= 1
+    
+    return DateDelta(delta_years, delta_months, delta_days)
+
+
+class Memory(NamedTuple):
+    id: int
+    date: str
+    name: str
+    age: DateDelta
+    note: str
+
+
+def get_memories(cur: sqlite3.Cursor, name: str) -> list[Memory]:
     res = cur.execute(
-        "SELECT id, date, name, note FROM memories WHERE name = ? ORDER BY date, id",
-        (request.match_info.get("name"), ),
-    )
+        "SELECT id, date, note FROM memories WHERE name = ? ORDER BY date, id",
+        (name, ),
+    ).fetchall()
     
-    out = [
-        {
-            "id": id,
-            "date": date,
-            "name": name,
-            "note": note,
-        }
-        for id, date, name, note in res.fetchall()
+    if len(res) == 0:
+      return []
+    
+    # Assume DoB is the date of the first memory
+    dob = datetime.date.fromisoformat(res[0][1])
+    
+    return [
+        Memory(
+            id=id,
+            date=date,
+            name=name,
+            age=date_delta(dob, datetime.date.fromisoformat(date)),
+            note=note,
+        )
+        for id, date, note in res
     ]
+
+def get_memory(cur: sqlite3.Cursor, id: int) -> list[Memory]:
+    date, name, note = cur.execute(
+        "SELECT date, name, note FROM memories WHERE id = ?",
+        (id, ),
+    ).fetchone()
     
-    if out:
-        dob = datetime.date.fromisoformat(out[0]["date"])
-        
-        for entry in out:
-            now = datetime.date.fromisoformat(entry["date"])
-            
-            # Count number of months between two dates (disregarding days)
-            year = dob.year
-            month = dob.month
-            delta_months = 0
-            while year < now.year or month < now.month:
-                delta_months += 1
-                month += 1
-                if month >= 13:
-                  month -= 12
-                  year += 1
-            delta_years = delta_months // 12
-            delta_months -= delta_years * 12
-            
-            # Compute differences in days and borrow months and years as
-            # necessary
-            delta_days = now.day - dob.day
-            if delta_days < 0:
-                delta_days += monthrange(dob.year, dob.month)[1]
-                delta_months -= 1
-            if delta_months < 0:
-                delta_months += 12
-                delta_years -= 1
-            
-            entry["days"] = delta_days
-            entry["months"] = delta_months
-            entry["years"] = delta_years
+    dob, = cur.execute(
+        "SELECT date FROM memories WHERE name = ? ORDER BY date, id LIMIT 1",
+        (name, ),
+    ).fetchone()
     
-    return web.json_response(out)
+    return Memory(
+        id=id,
+        date=date,
+        name=name,
+        age=date_delta(datetime.date.fromisoformat(dob), datetime.date.fromisoformat(date)),
+        note=note,
+    )
 
 
-@routes.post("/api/memories/{name}")
-async def post_memory(request: web.Request) -> web.Response:
+def group_memories(memories: list[Memory]) -> OrderedDict[tuple[int, int], list[Memory]]:
     """
-    Add a new memory to the database.
+    Group memories by ages quantised to (year, month).
+    """
+    memories_by_month = OrderedDict()
+    
+    last_age_months = None
+    memories_this_month = None
+    for memory in memories:
+        age_months = (memory.age.years, memory.age.months)
+        if age_months != last_age_months:
+            memories_by_month[age_months] = memories_this_month = []
+            last_age_months = age_months
+        
+        memories_this_month.append(memory)
+    
+    return memories_by_month
+
+
+def format_years_months(age: tuple[int, int]) -> str:
+    """
+    Format an age given as a tuple (years, months) as a string like "3 years, 1
+    month".
+    """
+    years, months = age[:2]
+    
+    if years == 0 and months == 0:
+        return "newborn"
+    elif years == 0:
+        return f"{months} month{'s' if months != 1 else ''}"
+    elif months == 0:
+        return f"{years} year{'s' if years != 1 else ''}"
+    else:
+        return (
+            f"{years} year{'s' if years != 1 else ''}, "
+            f"{months} month{'s' if months != 1 else ''}"
+        )
+
+
+@routes.get("/memories/{name}")
+async def memories(request: web.Request) -> web.Response:
+    name = request.match_info["name"]
+    
+    variables = {
+        "name": name,
+        "today": datetime.date.today().isoformat(),
+        "memories_by_month": group_memories(get_memories(request.app["db"].cursor(), name)),
+    }
+    
+    return render_template(request, "memories.html", variables)
+
+
+@routes.post("/memories/{name}")
+async def post_memories(request: web.Request) -> web.Response:
+    """
+    POSTing to this endpoint will either insert, modify or delete a memory
+    depending on whether 'id' is absent (insert) or present (edit) or the
+    only value (delete).
     """
     data = await request.post()
     
     name = request.match_info["name"]
-    date = data["date"]
-    note = data["note"]
+    id = data.get("id")  # None iff creating a new memory
     
-    cur = request.app["db"].cursor()
-    res = cur.execute("INSERT INTO memories(date, name, note) VALUES(?, ?, ?)", (date, name, note))
-    request.app["db"].commit()
+    if "date" in data and "note" in data:
+        # Create or edit
+        date = data["date"]
+        note = data["note"]
+        
+        # Simple validation
+        if not name.strip():
+            raise web.HTTPBadRequest(text="Name was empty.")
+        if not re.match(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", date):
+            raise web.HTTPBadRequest(text=f"Invalid date: {date!r}, expected YYYY-MM-DD")
+        if not note.strip():
+            raise web.HTTPBadRequest(text="Note was empty.")
+        
+        cur = request.app["db"].cursor()
+        
+        if id is None:
+            res = cur.execute("INSERT INTO memories(date, name, note) VALUES(?, ?, ?)", (date, name, note))
+            id = cur.lastrowid
+        else:
+            res = cur.execute("UPDATE memories SET date = ?, note = ? WHERE id = ?", (date, note, id))
+        
+        request.app["db"].commit()
     
-    return web.json_response(cur.lastrowid)
+        raise web.HTTPSeeOther(f"{request.url.path}#memory-{id}")
+    elif id is not None:
+        # Delete
+        cur = request.app["db"].cursor()
+        memory = get_memory(cur, id)
+        
+        cur.execute("DELETE FROM memories WHERE id = ?", (id, ));
+        request.app["db"].commit()
+        
+        raise web.HTTPSeeOther(f"{request.url.path}#age-{memory.age.years}-{memory.age.months}")
+
+
+@routes.get(r"/edit/{id:\d+}")
+async def memories(request: web.Request) -> web.Response:
+    id = request.match_info["id"]
+    
+    memory = get_memory(request.app["db"].cursor(), id)
+    
+    return render_template(request, "edit.html", {"memory": memory})
+
 
 routes.static("/static", Path(__file__).parent / "static")
 
 
-async def database(app: web.Application):
+app = web.Application()
+app.add_routes(routes)
+
+
+@app.cleanup_ctx.append
+async def db(app: web.Application):
     app["db"] = sqlite3.connect("memories.db")
     
     cur = app["db"].cursor()
@@ -122,14 +249,20 @@ async def database(app: web.Application):
     )
     app["db"].commit()
     
-    yield
+    try:
+        yield
+    finally:
+        app["db"].close()
+
+
+@app.on_startup.append
+async def env(app: web.Application):
+    app["env"] = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(Path(__file__).parent / "templates"),
+        autoescape=jinja2.select_autoescape(),
+    )
     
-    app["db"].close()
-
-
-app = web.Application()
-app.add_routes(routes)
-app.cleanup_ctx.append(database)
+    app["env"].filters["format_years_months"] = format_years_months
 
 
 if __name__ == "__main__":
